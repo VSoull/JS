5eb13547c527c3e69fdfbc0474b5a91c6dabe5bd
Valentin, Hetriuc <valentinconstantin.hetriuc@cgm.com>
2017-09-05 17:02:25 +0300

    finished iterator

--- javascript-ex/iterator.js
+++ javascript-ex/iterator.js
@@ -1,25 +1,65 @@
 console.log('enter program');
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var index = 0;
 
 //iterator
+var it = (function iterator(arrayArg) {
+    var index = 0;
 
-function iterator(arrayArg) {
+    function hasElement() {
+        if (index <= arrayArg.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function errorMsg(message) {
+        throw new Error(message);
+    }
 
     function getNext() {
-        return arrayArg[index++];
+        if (hasElement()) {
+            return arrayArg[index++];
+        } else {
+            errorMsg('Index out of bounds.');
+        }
     }
 
     function getPrevious() {
-        return arrayArg[index--];
+        if (hasElement()) {
+            return arrayArg[--index];
+        } else {
+            errorMsg('Index out of bounds.');
+        }
     }
 
-    return getNext();
+    var iterator = {
+        getNext: getNext,
+        getPrevious: getPrevious
+    };
+    return iterator;
+})(array);
+
+// define("iterator", function(arrayArg) {
+//     var index = 0;
+//     return {
+//         getNext: function() {
+//             return arrayArg[index++];
+//         },
+
+//         getPrevious: function() {
+//             return arrayArg[index--];
+//         }
+//     };
+// });
+
+for (var i = 0; i < array.length; i++) {
+    console.log('iterator getNext ' + it.getNext());
 }
 
 
-for (var number in array) {
-    console.log('iterator getNext ' + iterator(array));
+for (var i = 0; i <= array.length - 1; i++) {
+    console.log('iterator Prev ' + it.getPrevious());
 }
 
 
