d9858d2c4bbcc6d5455369952d66d3df20da7fff
Valentin, Hetriuc <valentinconstantin.hetriuc@cgm.com>
2017-09-28 17:54:21 +0300

    refactor main file with ES6

--- javascript-ex/main.js
+++ javascript-ex/main.js
@@ -5,15 +5,20 @@ console.log('start program');
 let arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let arrayString = ['1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a'];
 
+const iterate = (array, fn) => {
+    for (const element of array) {
+        console.log(' ---> ' + fn());
+    }
+};
+
 let it = iterator(arrayNumber);
 
-arrayNumber.forEach((element) => console.log('next ' + it.getNext()));
-arrayNumber.forEach((element) => console.log('prev ' + it.getPrevious()));
+iterate(arrayNumber, it.getNext);
+iterate(arrayNumber, it.getPrevious);
 
 console.log("---------------------------------------------------------");
 it = iterator(arrayString);
-
-arrayString.forEach((element) => console.log('next ' + it.getNext()));
-arrayString.forEach((element) => console.log('prev ' + it.getPrevious()));
+iterate(arrayString, it.getNext);
+iterate(arrayString, it.getPrevious);
 
 console.log('end program');
