26c2309e0e1941266b299c2162649866284bff5c
Valentin, Hetriuc <valentinconstantin.hetriuc@cgm.com>
2017-09-28 17:59:40 +0300

    refactor main file with ES6 for -of

--- javascript-ex/main.js
+++ javascript-ex/main.js
@@ -6,8 +6,8 @@ let arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let arrayString = ['1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a'];
 
 const iterate = (array, fn) => {
-    for (const element of array) {
-        console.log(' ---> ' + fn());
+    for (const [key, value] of array.entries()) {
+        console.log(`Key = ${key}, value = ${fn()} `);
     }
 };
 
